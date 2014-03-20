38ee6b76ebf48741a8d3db27c91fd84e100f9306
Satia Herfert <satia.herfert@dfki.de>
2014-03-20 15:54:49 +0100

    Script to produce a message delivery failure message for testing purposes.

---
+++ scripts/testFailureMsg.rb
@@ -0,0 +1,49 @@
+require 'orocos'
+
+include Orocos
+Orocos.initialize
+
+# Test producing a message delivery failure message
+Orocos.run "dlm_test", "fipa_services_test"  do  
+    
+    # Start a mts for the communication
+    begin
+        mts_module = TaskContext.get "mts_0"
+    rescue Orocos::NotFound
+        print 'Deployment not found.'
+        raise
+    end
+    mts_module.configure
+    mts_module.start
+    
+    agent1 = TaskContext.get "dlm_0"
+    agent2 = TaskContext.get "dlm_1" # will not actually be used
+
+    # load property from configuration file
+    agent1.apply_conf_file("distributed_locking_config.yml", ["ricart-agrawala", "agent1", "rsc1"])
+    agent2.apply_conf_file("distributed_locking_config.yml", ["ricart-agrawala", "agent2"])
+    
+    # register agents in the mts instead of connecting directly
+    mts_module.addReceiver("agent1", true)
+    # sleep so they can get published in avahi-discover
+    sleep 2
+    # and connect them to the mts ports
+    agent1.lettersOut.connect_to mts_module.letters, :type => :buffer, :size => 100
+    mts_module.agent1.connect_to agent1.lettersIn, :type => :buffer, :size => 100
+
+    # Call to configure is required for this component
+    # since it has been generated with 'needs_configuration'
+    agent1.configure
+    agent2.configure
+    
+    # start them
+    agent1.start
+    agent2.start
+    
+    resource = 'rsc1' # This is important due to the configuration with rsc1
+
+    # Now we lock, with a non-existing agent name
+    agent1.lock(resource, [agent2.getAgent])
+    # and wait until user presses Enter
+    readline
+end
