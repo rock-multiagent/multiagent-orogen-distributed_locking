adbc763b387372648a5acf7162543b3532dbc6a3
Satia Herfert <satia.herfert@dfki.de>
2014-03-25 14:52:13 +0100

    testFailure script now produces two different failure messages.

--- scripts/testFailureMsg.rb
+++ scripts/testFailureMsg.rb
@@ -17,7 +17,7 @@ Orocos.run "dlm_test", "fipa_services_test"  do
     mts_module.start
     
     agent1 = TaskContext.get "dlm_0"
-    agent2 = TaskContext.get "dlm_1" # will not actually be used
+    agent2 = TaskContext.get "dlm_1"
 
     # load property from configuration file
     agent1.apply_conf_file("distributed_locking_config.yml", ["ricart-agrawala", "agent1", "rsc1"])
@@ -44,6 +44,28 @@ Orocos.run "dlm_test", "fipa_services_test"  do
 
     # Now we lock, with a non-existing agent name
     agent1.lock(resource, [agent2.getAgent])
+    # and wait until user presses Enter
+    readline
+    
+    # now the agent gets added as a receiver
+    mts_module.addReceiver("agent2", true)
+    # sleep so they can get published in avahi-discover
+    sleep 2
+    # and connect them to the mts ports
+    agent2.lettersOut.connect_to mts_module.letters, :type => :buffer, :size => 100
+    mts_module.agent2.connect_to agent2.lettersIn, :type => :buffer, :size => 100
+    
+    # lock
+    agent1.lock(resource, [agent2.getAgent])
+    sleep 0.5
+    agent2.lock(resource, [agent1.getAgent])
+    sleep 0.5
+    # remove from receivers
+    mts_module.removeReceiver("agent2")
+    # sleep so they can get published in avahi-discover
+    sleep 2
+    # unlock (fails)
+    agent1.unlock(resource)
     # and wait until user presses Enter
     readline
 end
